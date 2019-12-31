065db083b0236ef4e1c94a58ce5fbe54ea53cd2e
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:17:16 -0600

    Rewriting as Laminas Project package

--- test/OfflineTest.php
+++ test/OfflineTest.php
@@ -1,25 +1,24 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-ldap for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-ldap/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-ldap/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Ldap;
+namespace LaminasTest\Ldap;
 
-use Zend\Config;
-use Zend\Ldap;
-use Zend\Ldap\Exception;
+use Laminas\Config;
+use Laminas\Ldap;
+use Laminas\Ldap\Exception;
 
 /**
- * @group      Zend_Ldap
+ * @group      Laminas_Ldap
  */
 class OfflineTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Zend\Ldap\Ldap instance
+     * Laminas\Ldap\Ldap instance
      *
      * @var Ldap\Ldap
      */
@@ -28,7 +27,7 @@ class OfflineTest extends \PHPUnit_Framework_TestCase
     /**
      * Setup operations run prior to each test method:
      *
-     * * Creates an instance of Zend\Ldap\Ldap
+     * * Creates an instance of Laminas\Ldap\Ldap
      *
      * @return void
      */
@@ -48,9 +47,9 @@ class OfflineTest extends \PHPUnit_Framework_TestCase
         $optionName = 'invalid';
         try {
             $this->ldap->setOptions(array($optionName => 'irrelevant'));
-            $this->fail('Expected Zend\Ldap\Exception\LdapException not thrown');
+            $this->fail('Expected Laminas\Ldap\Exception\LdapException not thrown');
         } catch (Exception\LdapException $e) {
-            $this->assertEquals("Unknown Zend\Ldap\Ldap option: $optionName", $e->getMessage());
+            $this->assertEquals("Unknown Laminas\Ldap\Ldap option: $optionName", $e->getMessage());
         }
     }
 
@@ -68,20 +67,20 @@ class OfflineTest extends \PHPUnit_Framework_TestCase
     public function testOptionsGetter()
     {
         $options = array(
-            'host'     => getenv('TESTS_ZEND_LDAP_HOST'),
-            'username' => getenv('TESTS_ZEND_LDAP_USERNAME'),
-            'password' => getenv('TESTS_ZEND_LDAP_PASSWORD'),
-            'baseDn'   => getenv('TESTS_ZEND_LDAP_BASE_DN'),
+            'host'     => getenv('TESTS_LAMINAS_LDAP_HOST'),
+            'username' => getenv('TESTS_LAMINAS_LDAP_USERNAME'),
+            'password' => getenv('TESTS_LAMINAS_LDAP_PASSWORD'),
+            'baseDn'   => getenv('TESTS_LAMINAS_LDAP_BASE_DN'),
         );
         $ldap    = new Ldap\Ldap($options);
         $this->assertEquals(array(
-                                 'host'                   => getenv('TESTS_ZEND_LDAP_HOST'),
+                                 'host'                   => getenv('TESTS_LAMINAS_LDAP_HOST'),
                                  'port'                   => 0,
                                  'useSsl'                 => false,
-                                 'username'               => getenv('TESTS_ZEND_LDAP_USERNAME'),
-                                 'password'               => getenv('TESTS_ZEND_LDAP_PASSWORD'),
+                                 'username'               => getenv('TESTS_LAMINAS_LDAP_USERNAME'),
+                                 'password'               => getenv('TESTS_LAMINAS_LDAP_PASSWORD'),
                                  'bindRequiresDn'         => false,
-                                 'baseDn'                 => getenv('TESTS_ZEND_LDAP_BASE_DN'),
+                                 'baseDn'                 => getenv('TESTS_LAMINAS_LDAP_BASE_DN'),
                                  'accountCanonicalForm'   => null,
                                  'accountDomainName'      => null,
                                  'accountDomainNameShort' => null,
@@ -98,20 +97,20 @@ class OfflineTest extends \PHPUnit_Framework_TestCase
     public function testConfigObject()
     {
         $config = new Config\Config(array(
-                                         'host'     => getenv('TESTS_ZEND_LDAP_HOST'),
-                                         'username' => getenv('TESTS_ZEND_LDAP_USERNAME'),
-                                         'password' => getenv('TESTS_ZEND_LDAP_PASSWORD'),
-                                         'baseDn'   => getenv('TESTS_ZEND_LDAP_BASE_DN'),
+                                         'host'     => getenv('TESTS_LAMINAS_LDAP_HOST'),
+                                         'username' => getenv('TESTS_LAMINAS_LDAP_USERNAME'),
+                                         'password' => getenv('TESTS_LAMINAS_LDAP_PASSWORD'),
+                                         'baseDn'   => getenv('TESTS_LAMINAS_LDAP_BASE_DN'),
                                     ));
         $ldap   = new Ldap\Ldap($config);
         $this->assertEquals(array(
-                                 'host'                   => getenv('TESTS_ZEND_LDAP_HOST'),
+                                 'host'                   => getenv('TESTS_LAMINAS_LDAP_HOST'),
                                  'port'                   => 0,
                                  'useSsl'                 => false,
-                                 'username'               => getenv('TESTS_ZEND_LDAP_USERNAME'),
-                                 'password'               => getenv('TESTS_ZEND_LDAP_PASSWORD'),
+                                 'username'               => getenv('TESTS_LAMINAS_LDAP_USERNAME'),
+                                 'password'               => getenv('TESTS_LAMINAS_LDAP_PASSWORD'),
                                  'bindRequiresDn'         => false,
-                                 'baseDn'                 => getenv('TESTS_ZEND_LDAP_BASE_DN'),
+                                 'baseDn'                 => getenv('TESTS_LAMINAS_LDAP_BASE_DN'),
                                  'accountCanonicalForm'   => null,
                                  'accountDomainName'      => null,
                                  'accountDomainNameShort' => null,
