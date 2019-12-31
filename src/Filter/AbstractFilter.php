92c83779b5ab3f4a43221a7831bc872d66d81dc9
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:16:21 -0600

    Rewriting as Laminas Project package

--- src/Filter/AbstractFilter.php
+++ src/Filter/AbstractFilter.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-ldap for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-ldap/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-ldap/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Ldap\Filter;
+namespace Laminas\Ldap\Filter;
 
-use Zend\Ldap\Converter\Converter;
+use Laminas\Ldap\Converter\Converter;
 
 /**
- * Zend\Ldap\Filter\AbstractFilter provides a base implementation for filters.
+ * Laminas\Ldap\Filter\AbstractFilter provides a base implementation for filters.
  */
 abstract class AbstractFilter
 {
--- src/Filter/Exception/ExceptionInterface.php
+++ src/Filter/Exception/ExceptionInterface.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-ldap for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-ldap/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-ldap/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Ldap\Filter\Exception;
+namespace Laminas\Ldap\Filter\Exception;
 
-use Zend\Ldap\Exception\ExceptionInterface as MainLdapException;
+use Laminas\Ldap\Exception\ExceptionInterface as MainLdapException;
 
 interface ExceptionInterface extends MainLdapException
 {
