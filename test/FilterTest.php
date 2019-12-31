b48b19af6713ff424773055fa2ad388163b10397
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:15:49 -0600

    Rewriting as Laminas Project package

--- test/FilterTest.php
+++ test/FilterTest.php
@@ -1,23 +1,21 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Ldap
+ * @see       https://github.com/laminas/laminas-ldap for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-ldap/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-ldap/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Ldap;
+namespace LaminasTest\Ldap;
 
-use Zend\Ldap;
-use Zend\Ldap\Filter;
+use Laminas\Ldap;
+use Laminas\Ldap\Filter;
 
 /**
- * @category   Zend
- * @package    Zend_Ldap
+ * @category   Laminas
+ * @package    Laminas_Ldap
  * @subpackage UnitTests
- * @group      Zend_Ldap
+ * @group      Laminas_Ldap
  */
 class FilterTest extends \PHPUnit_Framework_TestCase
 {
@@ -105,7 +103,7 @@ class FilterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException Zend\Ldap\Filter\Exception\FilterException
+     * @expectedException Laminas\Ldap\Filter\Exception\FilterException
      */
     public function testIllegalGroupingFilter()
     {
