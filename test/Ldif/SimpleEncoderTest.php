c65008a8d4ca4de59558d1c452363d015acdf86b
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:15:52 -0600

    Rewriting as Laminas Project package

--- test/Ldif/SimpleEncoderTest.php
+++ test/Ldif/SimpleEncoderTest.php
@@ -1,24 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Ldap
+ * @see       https://github.com/laminas/laminas-ldap for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-ldap/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-ldap/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Ldap\Ldif;
+namespace LaminasTest\Ldap\Ldif;
 
-use Zend\Ldap\Ldif;
-use ZendTest\Ldap as TestLdap;
+use Laminas\Ldap\Ldif;
+use LaminasTest\Ldap as TestLdap;
 
 /**
- * @category   Zend
- * @package    Zend_Ldap
+ * @category   Laminas
+ * @package    Laminas_Ldap
  * @subpackage UnitTests
- * @group      Zend_Ldap
- * @group      Zend_Ldap_Ldif
+ * @group      Laminas_Ldap
+ * @group      Laminas_Ldap_Ldif
  */
 class SimpleEncoderTest extends TestLdap\AbstractTestCase
 {
