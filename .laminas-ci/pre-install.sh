94e7f615fcedb1e14b44bab9b185930d76cb0885
Kevin Papst <kpapst@gmx.net>
2022-02-10 13:11:27 +0100

    install iptables
    Signed-off-by: Kevin Papst <kpapst@gmx.net>

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -5,7 +5,7 @@ JOB=$3
 PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
 
 apt install -y php8.1-ldap || exit 1
-
+apt install -y iptables || exit 1
 apt install -y slapd ldap-utils || exit 1
 
 ./.ci/config_iptables.sh
