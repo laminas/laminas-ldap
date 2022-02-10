b6c9719c422899612dd138bf8bf543acb10f83dd
Kevin Papst <kpapst@gmx.net>
2022-02-10 13:56:39 +0100

    trying to fix iptables rules

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -4,10 +4,10 @@ WORKING_DIRECTORY=$2
 JOB=$3
 PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
 
-#apt-get install -y php8.1-ldap || exit 1
 apt-get install -y iptables conntrack || exit 1
+sudo ./.ci/config_iptables.sh
+
 apt-get install -y slapd ldap-utils || exit 1
 
-sudo ./.ci/config_iptables.sh
 ./.ci/OpenLDAP_run.sh
 ./.ci/load_fixtures.sh
